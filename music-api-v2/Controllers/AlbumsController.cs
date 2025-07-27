008d8539d2f550fe7bb329d1b54a395ec6185805
hiroji <joegilbertdev@outlook.com>
2025-07-27 12:17:25 +0100

    Updated controllers to display response interface

--- music-api-v2/Controllers/AlbumsController.cs
+++ music-api-v2/Controllers/AlbumsController.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using music_api_v2.Models;
 using music_api_v2.Services;
 
 namespace music_api_v2.Controllers;
 
 public class AlbumsController(IAlbumService albumService) : ControllerBase
 {   
+    /// <summary>
+    /// Gets a list of albums
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     [Route("/api/albums")]
-    public async Task<IActionResult> GetAlbums()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<AlbumRow[]>> GetAlbums()
     {
         var albums = await albumService.HandleGetAllAlbumRows();
         return Ok(albums);
--- music-api-v2/Controllers/PingController.cs
+++ music-api-v2/Controllers/PingController.cs
@@ -4,9 +4,14 @@ namespace music_api_v2.Controllers;
 
 public class PingController : ControllerBase
 {   
+    /// <summary>
+    /// Returns string Pong if server is extant
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     [Route("api/ping")]
-    public IActionResult Ping()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult<string> Ping()
     {
         return Ok("Pong");
     }
